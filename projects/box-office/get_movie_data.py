c254774d057762c537e05bb32137a33c7e9e2461
Simon Bedford <simon.bedford@gmail.com>
2016-05-03 21:58:40 -0500

    Modified scraper to reduce HTTP 403 exceptions.
    -Using random choice of user-agent
    -Reduced chunk size to 200 and greater throttling of requests
    -Writing data to file as processed

--- projects/box-office/get_movie_data.py
+++ projects/box-office/get_movie_data.py
@@ -1,44 +1,64 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from urllib.request import Request
+from random import choice
 import re
 
 
+HEADERS = [
+    {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"},
+    {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'},
+    {"User-Agent": 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'},
+    {"User-Agent": 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12'}
+]
+
 def movie_data(filmID):
     url = "http://www.boxofficemojo.com/movies/?page=weekend&" + filmID + "&adjust_yr=2015&p=.htm"
+    headers = choice(HEADERS)
+    page = Request(url, headers=headers)
 
-    soup = BeautifulSoup(urlopen(url).read(), "lxml")
+    soup = BeautifulSoup(urlopen(page).read(), "lxml", from_encoding="utf-8")
     body = soup.find('div', {'id': 'body'})
 
     general = [filmID]
     detailed = []
 
     # Domestic Total Gross
-    if body.find(string=re.compile("Domestic Total")):
-        dtg = body.find(string=re.compile("Domestic Total")).find_parent().find('b').contents[0]
+    domestic_total = body.find(string=re.compile("Domestic Total"))
+    if domestic_total and domestic_total.find_parent():
+        dtg = body.find(string=re.compile("Domestic Total")).find_parent().b.string
     else:
         dtg = "NA"
     general.append(dtg)
     # Release Date
-    if body.find(string=re.compile("Release Date")):
-        rel_date = body.find(string=re.compile("Release Date")).find_parent().find('a')["href"]
-        general.append(re.search('&date=(.+)&', rel_date).groups()[0])
+    release_date = body.find(string=re.compile("Release Date"))
+    if release_date and release_date.find_parent() and release_date.find_parent().a:
+        rel_date = body.find(string=re.compile("Release Date")).find_parent().a["href"]
+        if re.search('&date=(.+)&', rel_date):
+            dte = re.search('&date=(.+)&', rel_date).groups()[0]
+        else:
+            dte = "NA"
     else:
-        general.append("NA")
+        dte = "NA"
+    general.append(dte)
     # Distributor
-    if body.find(string=re.compile("Distributor")):
-        distributor = body.find(string=re.compile("Distributor")).find_parent().find('a').contents[0]
+    distrib = body.find(string=re.compile("Distributor"))
+    if distrib and distrib.find_parent():
+        distributor = body.find(string=re.compile("Distributor")).find_parent().a.string
     else:
         distributor = "NA"
     general.append(distributor)
     # Genre
-    if body.find(string=re.compile("Genre")):
-        genre = body.find(string=re.compile("Genre")).find_parent().find('b').contents[0]
+    gen = body.find(string=re.compile("Genre"))
+    if gen and gen.find_parent():
+        genre = body.find(string=re.compile("Genre")).find_parent().b.string
     else:
         genre = "NA"
     general.append(genre)
     # Production Budget
-    if body.find(string=re.compile("Production Budget")):
-        budget = body.find(string=re.compile("Production Budget")).find_parent().find('b').contents[0]
+    production_budget = body.find(string=re.compile("Production Budget"))
+    if production_budget and production_budget.find_parent():
+        budget = body.find(string=re.compile("Production Budget")).find_parent().b.string
     else:
         budget = "NA"
     general.append(budget)
@@ -46,7 +66,10 @@ def movie_data(filmID):
     # Weekend table data
     main = body.find('table', {'class': 'chart-wide'})
     if main:
-        year = main.find_parent().find('b').find('font').contents[0]
+        if main.find_parent() and main.find_parent().b and main.find_parent().b.font:
+            year = main.find_parent().b.font.string
+        else:
+            year = "NA"
 
         rows1 = main.findAll('tr', {'bgcolor': '#ffffff'})
         rows2 = main.findAll('tr', {'bgcolor': '#f4f4ff'})
--- projects/box-office/run_scraper.py
+++ projects/box-office/run_scraper.py
@@ -1,6 +1,22 @@
 import time
 import csv
 from get_movie_data import movie_data
+import urllib
+
+
+def write_files(general, details):
+    with open('general-info.csv', "a", newline='') as f:
+        writer = csv.writer(f)
+        # writer.writerow(['movieID', 'AdjustedDomesticGross', 'ReleaseDate', 'Distributor', 'Genre', 'Budget'])
+        for row in general:
+            writer.writerow(row)
+
+    with open('weekend-info.csv', "a", newline='') as f:
+        writer = csv.writer(f)
+        # writer.writerow(['movieID', 'Year', 'Date', 'Rank', 'WeekendGross', 'pcChange','Theaters','Change','Avg','Gross-to-Date','Week_num'])
+        for row in details:
+            writer.writerow(row)
+
 
 movies = []
 
@@ -10,31 +26,32 @@ with open("movie_list.csv", "r") as f:
     for d in data:
         movies.append(d)
 
-general = []
-details = []
+# Skip the first N movies
+n = 1199
+new_movies = movies[n + 1:]
 
-# Chunk movie list into 1000 films at a time
-chunks = [movies[x: x + 1000] for x in range(0, len(movies), 1000)]
+
+# Chunk movie list into 200 films at a time
+chunks = [new_movies[x: x + 200] for x in range(0, len(movies), 200)]
+# indices = [j for j in range(len(movies))]
+# chunks = [[movies[i] for i in np.random.choice(indices, 100)]]
+
+# Set which chunk we need to process
+# start = 0
 
 for ch in chunks:
     for c in ch:
-        gen, det = movie_data(c[1])
-        general.append(gen)
-        for d in det:
-            details.append(d)
-        # Pause 1 second between movies
-        time.sleep(1)
-    # Pause 5 mins between chunks
-    time.sleep(5 * 60)
-
-with open('general-info.csv', "w") as f:
-    writer = csv.writer(f)
-    writer.writerow(['movieID', 'AdjustedDomesticGross', 'ReleaseDate', 'Distributor', 'Genre', 'Budget'])
-    for row in general:
-        writer.writerow(row)
-
-with open('weekend-info.csv', "w") as f:
-    writer = csv.writer(f)
-    writer.writerow(['movieID', 'Date', 'Rank', 'WeekendGross', 'pcChange','Theaters','Change','Avg','Gross-to-Date','Rank'])
-    for row in details:
-        writer.writerow(row)
+        general = []
+        details = []
+        try:
+            gen, det = movie_data(c[1])
+            general.append(gen)
+            for d in det:
+                details.append(d)
+            write_files(general, details)
+        except urllib.error.HTTPError:
+            break
+        else:
+            # Pause 3 seconds between movies
+            time.sleep(3)
+    time.sleep(30)
