195fb3b80faa7ae6fb73d0aee16d2f51d3c4e6aa
Simon Bedford <simon.bedford@gmail.com>
2016-08-28 10:22:17 -0500

    Shuffle augmented images.

--- capstone/transfer-learning/code/reduced_augment_images.py
+++ capstone/transfer-learning/code/reduced_augment_images.py
@@ -10,9 +10,18 @@ import re
 parser = argparse.ArgumentParser(description='augment image dataset')
 parser.add_argument('images', metavar='I', type=str, nargs=1,
     help='Text file containing list of images')
+parser.add_argument('image_path', metavar='P', type=str, nargs=1,
+    help='Path for where base images are saved')
+parser.add_argument('image_output_path', metavar='O', type=str, nargs=1,
+    help='Path for storing augmented images')
+parser.add_argument('image_list_path', metavar='L', type=str, nargs=1,
+    help='Path for saving text file list of images')
 
 args = parser.parse_args()
 images = args.images[0]
+image_out = args.image_output_path[0]
+image_path = args.image_path[0]
+data_out = args.image_list_path[0]
 
 if not os.path.exists(images):
     print ("Image file not valid. Please pass path to valid txt file.")
@@ -33,75 +42,76 @@ for x in image_list:
 
     # Add original image to list of augmented images
     #Load the image
-    img = io.imread(os.path.join("../data/resized", im_path))
+    img = io.imread(os.path.join(image_path, im_path))
     #Save the original image
-    io.imsave(os.path.join("/data/images", full_im_name), img)
+    io.imsave(os.path.join(image_out, full_im_name), img)
     new_image_list.append(full_im_name + " " + label)
-    io.imsave(os.path.join("/data/images", "m_" + full_im_name), np.fliplr(img))
+    io.imsave(os.path.join(image_out, "m_" + full_im_name), np.fliplr(img))
     new_image_list.append("m_" + full_im_name + " " + label)
     #Perform transformations
     # Lighten 1 & mirror image
     new_img = exposure.adjust_gamma(img, gamma=0.45)
     new_name = im_name + "_p1" + ".jpg"
     new_image_list.append(new_name + " " + label)
-    io.imsave(os.path.join("/data/images", new_name), new_img)
+    io.imsave(os.path.join(image_out, new_name), new_img)
     new_name = im_name + "_p1m" + ".jpg"
     new_image_list.append(new_name + " " + label)
-    io.imsave(os.path.join("/data/images", new_name), np.fliplr(new_img))
+    io.imsave(os.path.join(image_out, new_name), np.fliplr(new_img))
 
     # Lighten 2 & mirror image
     new_img = exposure.adjust_gamma(img, gamma=0.65)
     new_name = im_name + "_p2" + ".jpg"
     new_image_list.append(new_name + " " + label)
-    io.imsave(os.path.join("/data/images", new_name), new_img)
+    io.imsave(os.path.join(image_out, new_name), new_img)
     new_name = im_name + "_p2m" + ".jpg"
-    io.imsave(os.path.join("/data/images", new_name), np.fliplr(new_img))
+    io.imsave(os.path.join(image_out, new_name), np.fliplr(new_img))
 
     # Lighten 3 & mirror image
     new_img = exposure.adjust_gamma(img, gamma=0.85)
     new_name = im_name + "_p3" + ".jpg"
     new_image_list.append(new_name + " " + label)
-    io.imsave(os.path.join("/data/images", new_name), new_img)
+    io.imsave(os.path.join(image_out, new_name), new_img)
     new_name = im_name + "_p3m" + ".jpg"
     new_image_list.append(new_name + " " + label)
-    io.imsave(os.path.join("/data/images", new_name), np.fliplr(new_img))
+    io.imsave(os.path.join(image_out, new_name), np.fliplr(new_img))
 
     # Darken 1 & mirror image
     new_img = exposure.adjust_gamma(img, gamma=1.25)
     new_name = im_name + "_p4" + ".jpg"
     new_image_list.append(new_name + " " + label)
-    io.imsave(os.path.join("/data/images", new_name), new_img)
+    io.imsave(os.path.join(image_out, new_name), new_img)
     new_name = im_name + "_p4m" + ".jpg"
     new_image_list.append(new_name + " " + label)
-    io.imsave(os.path.join("/data/images", new_name), np.fliplr(new_img))
+    io.imsave(os.path.join(image_out, new_name), np.fliplr(new_img))
 
     # Darken 2 & mirror image
     new_img = exposure.adjust_gamma(img, gamma=1.5)
     new_name = im_name + "_p5" + ".jpg"
     new_image_list.append(new_name + " " + label)
-    io.imsave(os.path.join("/data/images", new_name), new_img)
+    io.imsave(os.path.join(image_out, new_name), new_img)
     new_name = im_name + "_p5m" + ".jpg"
     new_image_list.append(new_name + " " + label)
-    io.imsave(os.path.join("/data/images", new_name), np.fliplr(new_img))
+    io.imsave(os.path.join(image_out, new_name), np.fliplr(new_img))
 
     # Darken 3 & mirror image
     new_img = exposure.adjust_gamma(img, gamma=2)
     new_name = im_name + "_p6" + ".jpg"
     new_image_list.append(new_name + " " + label)
-    io.imsave(os.path.join("/data/images", new_name), new_img)
+    io.imsave(os.path.join(image_out, new_name), new_img)
     new_name = im_name + "_p6m" + ".jpg"
     new_image_list.append(new_name + " " + label)
-    io.imsave(os.path.join("/data/images", new_name), np.fliplr(new_img))
+    io.imsave(os.path.join(image_out, new_name), np.fliplr(new_img))
 
     # Rotate 180 degrees & mirror image
     new_img = transform.rotate(img, 180)
     new_name = im_name + "_p7" + ".jpg"
     new_image_list.append(new_name + " " + label)
-    io.imsave(os.path.join("/data/images", new_name), new_img)
+    io.imsave(os.path.join(image_out, new_name), new_img)
     new_name = im_name + "_p7m" + ".jpg"
     new_image_list.append(new_name + " " + label)
-    io.imsave(os.path.join("/data/images", new_name), np.fliplr(new_img))
+    io.imsave(os.path.join(image_out, new_name), np.fliplr(new_img))
 
+np.random.shuffle(new_image_list)
 
-with open(os.path.join("../data/alexnet_4", "augmented_" + re.findall(r'\/(\w+\.txt)$', images)[0]), "w") as f:
+with open(os.path.join(data_out, "augmented_" + re.findall(r'\/(\w+\.txt)$', images)[0]), "w") as f:
     f.write("\n".join(new_image_list))
